6025f04ad57143734ed43232d5a193fce9218a75
kurushiba <156669290+kurushiba@users.noreply.github.com>
2026-04-20 18:45:38 +0900

    Delete src directory

--- src/modules/auth/current-user.state.ts
+++
@@ -1,4 +0,0 @@
-import { atom } from 'jotai';
-import { User } from '../users/user.entity';
-
-export const currentUserAtom = atom<User>();
--- src/modules/users/user.entity.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-  id!: string;
-  email!: string;
-  name!: string;
-  constructor(data: User) {
-    Object.assign(this, data);
-  }
-}
