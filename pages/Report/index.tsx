aaf7d19bbd61775f4917f13b7f1916d09d140861
kurushiba <156669290+kurushiba@users.noreply.github.com>
2026-05-05 17:09:43 +0900

    Update index.tsx

--- pages/Report/index.tsx
+++ pages/Report/index.tsx
@@ -1,5 +1,16 @@
 import './index.css';
 
+// const TASK_COLORS = [
+//   '#e74c3c',
+//   '#3498db',
+//   '#2ecc71',
+//   '#f39c12',
+//   '#9b59b6',
+//   '#1abc9c',
+//   '#e67e22',
+//   '#34495e',
+// ];
+
 export default function Report() {
   // ローディング状態のUIを確認する場合はこちらをコメントイン:
   // return (
